af1de81b61fb6a861421f36c2453ff0c6b772026
Eduard Voicu <iameduardvoicu@gmail.com>
2021-08-22 16:18:31 -0500

    created const by pulling from the html index

---
+++ script.js
@@ -0,0 +1,8 @@
+const answersTrackerContainer = document.querySelector(".answers-tracker")
+const options = document.querySelector(".options").children
+const questionNumberSpan = document.querySelector(".question-num-value")
+const question=document.querySelector(".question")
+const totalQuestionsSpan =document.querySelector(".total-questions")
+const correctAnswersSpan =document.querySelector(".correct-answers")
+const totalQuestionsSpan2 =document.querySelector(".total-questions2")
+const percentageSpan =document.querySelector(".percentage")
